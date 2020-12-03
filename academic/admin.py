aed5d6bf6ca1776f159a70cf0df96b886eccb349
Krunal Atul Moharkar <62883820+krunalM05@users.noreply.github.com>
2020-12-03 11:47:53 +0530

    Add files via upload

---
+++ academic/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from academic.models import *
+
+# Register your models here.
+admin.site.register(UploadCalender)
+admin.site.register(TimeTable)
+admin.site.register(SemOne)
+'''admin.site.register(SemTwo)
+admin.site.register(SemThree)
+admin.site.register(SemFour)'''
